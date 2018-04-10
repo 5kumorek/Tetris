69c092aeec3a4c412d1530067b3617acc4f34608
Amadeusz <xaaq333@gmail.com>
2018-04-10 17:43:16 +0200

    create main menu class

---
+++ core/src/com/tetris/main_classes/MainMenuScreen.java
@@ -0,0 +1,47 @@
+package com.tetris.main_classes;
+
+import com.badlogic.gdx.Screen;
+
+public class MainMenuScreen implements Screen {
+
+    private final MainController controller;
+
+    MainMenuScreen(MainController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
