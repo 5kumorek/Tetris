1ad62ea717b9cec919448038ee2e05da279f96fd
Amadeusz <xaaq333@gmail.com>
2018-04-10 21:31:56 +0200

    create create constructor and render method of Board

--- core/src/com/tetris/other_classes/Board.java
+++ core/src/com/tetris/other_classes/Board.java
@@ -1,5 +1,16 @@
 package com.tetris.other_classes;
 
-public class Board implements IBoard
-{
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Board implements IBoard {
+    private Texture texture;
+
+    public Board(int width, int height) {
+        texture = new Texture("badlogic.jpg");
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, 0, 0);
+    }
 }
