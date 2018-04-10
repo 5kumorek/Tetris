d8ed0c042ff6241e5d94cb85721f1c126d0f5940
Amadeusz <xaaq333@gmail.com>
2018-04-10 17:44:56 +0200

    create game screen class

---
+++ core/src/com/tetris/main_classes/GameScreen.java
@@ -0,0 +1,46 @@
+package com.tetris.main_classes;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+    private final MainController controller;
+
+    GameScreen(MainController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
