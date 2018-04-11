e090dd238deedfc3c29784cf7824b07efba7ca9f
Shpiler7 <pcts79@gmail.com>
2018-04-11 17:09:06 +0200

    drawing tetrominoes on screen

--- core/src/com/tetris/game_utils/Board.java
+++ core/src/com/tetris/game_utils/Board.java
@@ -2,6 +2,7 @@ package com.tetris.game_utils;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.tetris.other_classes.IBoard;
 
 public class Board implements IBoard {
@@ -9,13 +10,26 @@ public class Board implements IBoard {
     private final int HEIGHT = 720;
 //    width and height are placeholder values for now - they can be changed if this class will be implemented more
     private Texture texture;
+    private ShapeRenderer shapeRenderer;
+    private Square block;
 
     public Board() {
         texture = new Texture("badlogic.jpg");
 //      this texture is only for testing purpouse, to see that this Board does anything
+        shapeRenderer = new ShapeRenderer();
+        block = new Square();
     }
 
     public void render(SpriteBatch batch) {
         batch.draw(texture, 0, 0);
+
+        //testing only
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(1, 1, 0, 1);
+        for(int i = 0; i < 4; i++)
+        {
+            shapeRenderer.rect(100 + 50*block.getX(i), 100 + 50*block.getY(i), 50, 50);
+        }
+        shapeRenderer.end();
     }
 }
--- core/src/com/tetris/game_utils/Square.java
+++ core/src/com/tetris/game_utils/Square.java
@@ -1,5 +1,37 @@
 package com.tetris.game_utils;
 
+import java.util.Random;
+
 public class Square implements Shape {
+
+    private int position[][];
+
+    public Square()
+    {
+        position = new int[4][2];
+        int [][][] coordinates = new int[][][] {
+                {{0, -1}, {0, 0}, {0, 1}, {0, 2}}, //I
+                {{0, 0}, {0, 1}, {1, 0}, {1, 1}}, //O
+                {{0, -1}, {0, 0}, {0, 1}, {1, 0}}, //T
+                {{0, -1}, {0, 0}, {1, 0}, {1, 1}}, //S
+                {{-1, 1}, {0, -1}, {0, 0}, {-1, 0}}, //Z
+                {{1, -1}, {0, -1}, {0, 0}, {0, 1}}, //J
+                {{-1, -1}, {0, -1}, {0, 0}, {0, 1}} //L
+        };
+
+        Random rand = new Random();
+        int randomPiece = Math.abs(rand.nextInt()) % 7;
+
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                position[i][j] = coordinates[randomPiece][i][j];
+            }
+        }
+    }
+
+    public int getX(int index) {return position[index][0];}
+    public int getY(int index) {return position[index][1];}
 
 }
