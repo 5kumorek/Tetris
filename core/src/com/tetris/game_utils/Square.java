b4fe9073e102b02a7f90b30ba41ab534ceacd3cf
Amadeusz <xaaq333@gmail.com>
2018-04-21 18:02:34 +0200

    add creating square texture

--- core/src/com/tetris/game_utils/Square.java
+++ core/src/com/tetris/game_utils/Square.java
@@ -1,4 +1,21 @@
 package com.tetris.game_utils;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+
 public class Square {
+    final static int PIXEL_SIZE = 50;
+    private Texture squareTexture;
+
+    public Square() {
+        createSquareTexture();
+    }
+
+    private void createSquareTexture() {
+        Pixmap pixmap = new Pixmap(PIXEL_SIZE, PIXEL_SIZE, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.RED);
+        pixmap.drawRectangle(0, 0, PIXEL_SIZE, PIXEL_SIZE);
+        squareTexture = new Texture(pixmap);
+    }
 }
