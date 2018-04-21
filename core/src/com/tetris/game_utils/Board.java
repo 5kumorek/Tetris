952b445a55e8c062f7eeec1e3b214df9b6dfff31
Amadeusz <xaaq333@gmail.com>
2018-04-21 21:14:32 +0200

    change creating new Figures to create by factory

--- core/src/com/tetris/game_utils/Board.java
+++ core/src/com/tetris/game_utils/Board.java
@@ -172,7 +172,7 @@ public class Board implements IBoard {
         FigureShape figureShapeValues[] = FigureShape.values();
         int randomNumber = new Random().nextInt(figureShapeValues.length);
         FigureShape randomFigureShape = figureShapeValues[randomNumber];
-        currentFigure = new Figure(ARRAY_WIDTH / 2, ARRAY_HEIGHT - 1, randomFigureShape);
+        currentFigure = figureFactory.getFigure(ARRAY_WIDTH / 2, ARRAY_HEIGHT - 1, randomFigureShape);
     }
 
     private void drawSquareArray(SpriteBatch batch) {
