41ca7f023fd6aa5370f78079faa0ca783c30a650
Amadeusz <xaaq333@gmail.com>
2018-04-21 20:54:58 +0200

    change output of FigureShape to Point

--- core/src/com/tetris/game_utils/FigureShape.java
+++ core/src/com/tetris/game_utils/FigureShape.java
@@ -1,5 +1,7 @@
 package com.tetris.game_utils;
 
+import java.awt.*;
+
 enum FigureShape {
     I(new int[][]{{0, -1}, {0, 0}, {0, 1}, {0, 2}}),
     O(new int[][]{{0, 0}, {1, 0}, {1, 1}, {0, 1}}),
@@ -9,18 +11,18 @@ enum FigureShape {
     J(new int[][]{{-1, -1}, {0, -1}, {0, 0}, {0, 1}}),
     L(new int[][]{{1, -1}, {0, -1}, {0, 0}, {0, 1}});
 
-    private final Square[] squareArray;
+    private final Point[] squareCoordinatesArray;
 
-    FigureShape(int[][] intArrayShape) {
-        squareArray = new Square[intArrayShape.length];
+    FigureShape(int[][] intCoordinatesArray) {
+        squareCoordinatesArray = new Point[intCoordinatesArray.length];
 
-        for (int i = 0; i < intArrayShape.length; i++) {
-            int coordinates[] = intArrayShape[i];
-            squareArray[i] = new Square(coordinates[0], coordinates[1]);
+        for (int i = 0; i < intCoordinatesArray.length; i++) {
+            int coordinates[] = intCoordinatesArray[i];
+            squareCoordinatesArray[i] = new Point(coordinates[0], coordinates[1]);
         }
     }
 
-    Square[] getSquareArray() {
-        return squareArray;
+    Point[] getSquareCoordinatesArray() {
+        return squareCoordinatesArray;
     }
 }
