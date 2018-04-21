7d3ca7ab4385edfbf275206a976c5e8f9b8b8e6e
Amadeusz <xaaq333@gmail.com>
2018-04-22 01:55:16 +0200

    delete reachedBottom method

--- core/src/com/tetris/game_utils/Figure.java
+++ core/src/com/tetris/game_utils/Figure.java
@@ -32,67 +32,7 @@ class Figure {
         return true;
     }
 
-    boolean reachedBottom() {
-        for (Square square : squareArray) {
-            if (square.getY() == 0)
-                return true;
-        }
-        return false;
-    }
-
     Square[] getSquareArray() {
         return squareArray;
     }
-
-
-//    private int position[][];
-//
-//    public Figure() {
-//        position = new int[4][2];
-//        int[][][] coordinates = new int[][][]{
-//                {{0, -1}, {0, 0}, {0, 1}, {0, 2}}, //I
-//                {{0, 0}, {0, 1}, {1, 0}, {1, 1}}, //O
-//                {{0, -1}, {0, 0}, {0, 1}, {1, 0}}, //T
-//                {{0, -1}, {0, 0}, {1, 0}, {1, 1}}, //S
-//                {{-1, 1}, {0, -1}, {0, 0}, {-1, 0}}, //Z
-//                {{1, -1}, {0, -1}, {0, 0}, {0, 1}}, //J
-//                {{-1, -1}, {0, -1}, {0, 0}, {0, 1}} //L
-//        };
-//
-//        Random rand = new Random();
-//        int randomPiece = Math.abs(rand.nextInt()) % 7;
-//
-//        for (int i = 0; i < 4; i++) {
-//            for (int j = 0; j < 2; j++) {
-//                position[i][j] = coordinates[randomPiece][i][j];
-//            }
-//        }
-//    }
-//
-//    public int getX(int index) {
-//        return position[index][0];
-//    }
-//
-//    public int getY(int index) {
-//        return position[index][1];
-//    }
-//
-//    public int minX() {
-//        int min = position[0][0];
-//        for (int i = 0; i < 4; i++) {
-//            if (position[i][0] < min)
-//                min = position[i][0];
-//        }
-//        return min;
-//    }
-//
-//    public int minY() {
-//        int min = position[0][1];
-//        for (int i = 0; i < 4; i++) {
-//            if (position[i][1] < min)
-//                min = position[i][1];
-//        }
-//        return min;
-//    }
-
 }
