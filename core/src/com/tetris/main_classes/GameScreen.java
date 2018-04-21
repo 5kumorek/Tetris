716bfff12f07ffa19dec70e0df6c09824a127e57
Amadeusz <xaaq333@gmail.com>
2018-04-22 01:04:56 +0200

    remove unnecessary newlines

--- core/src/com/tetris/main_classes/GameScreen.java
+++ core/src/com/tetris/main_classes/GameScreen.java
@@ -18,7 +18,6 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
-
     }
 
     @Override
@@ -31,26 +30,21 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
     }
 
     @Override
     public void pause() {
-
     }
 
     @Override
     public void resume() {
-
     }
 
     @Override
     public void hide() {
-
     }
 
     @Override
     public void dispose() {
-
     }
 }
