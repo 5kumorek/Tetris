32f4f97eab70f7c19da1f42b997ef9fb3efd9b9a
Amadeusz <xaaq333@gmail.com>
2018-04-21 17:45:13 +0200

    tidy up constants

--- core/src/com/tetris/game_utils/Board.java
+++ core/src/com/tetris/game_utils/Board.java
@@ -6,14 +6,13 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.tetris.other_classes.IBoard;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-
-import java.util.Arrays;
 
 public class Board implements IBoard {
-    private final int WIDTH = 300;
-    private final int HEIGHT = 660;
+    private final int PIXEL_WIDTH = 300;
+    private final int PIXEL_HEIGHT = 660;
+    private final int ARRAY_WIDTH = 10;
+    private final int ARRAY_HEIGHT = 20;
+
     //    width and height are placeholder values for now - they can be changed if this class will be implemented more
     private Texture boardTexture;
     private Texture texture;
@@ -21,8 +20,6 @@ public class Board implements IBoard {
     private Figure currentBlock;
     private int currentX = 0;
     private int currentY = 0;
-    private int BOARD_WIDTH = 10;
-    private int BOARD_HEIGHT = 20;
     private int step = 30;
     private boolean isDown = false;
     private boolean filledGrid[][];
@@ -34,70 +31,71 @@ public class Board implements IBoard {
 //      this texture is only for testing purpouse, to see that this Board does anything
         shapeRenderer = new ShapeRenderer();
         currentBlock = new Figure();
-        filledGrid = new boolean[BOARD_WIDTH][BOARD_HEIGHT];
+        filledGrid = new boolean[ARRAY_WIDTH][ARRAY_HEIGHT];
 
-        currentX = BOARD_WIDTH / 2 + 1;
-        currentY = BOARD_HEIGHT - 1 + currentBlock.minY();
+        currentX = ARRAY_WIDTH / 2 + 1;
+        currentY = ARRAY_HEIGHT - 1 + currentBlock.minY();
     }
 
     //TODO fix array -1 and Z/S shapes not falling correctly
     public void render(SpriteBatch batch) {
         //batch.draw(texture, 0, 0);
-        drawGrid();
-        if (isDown) {
-            isDown = false;
-            int removeLine = -1;
-            for (int i = 0; i < 4; i++) {
-                int x = currentX + currentBlock.getX(i);
-                int y = currentY + currentBlock.getY(i);
-                filledGrid[x - 1][y - currentBlock.minY()] = true;
-            }
-            int counter[] = new int[BOARD_HEIGHT];
-            for (int i = 0; i < BOARD_WIDTH; i++) {
-                for (int j = 0; j < BOARD_HEIGHT; j++) {
-                    if (filledGrid[i][j]) {
-                        counter[j]++;
-                    }
-                    if (counter[j] == 10)
-                        removeLine = j;
-                }
-            }
-            if (removeLine >= 0) {
-                for (int i = 0; i < BOARD_WIDTH; i++) {
-                    filledGrid[i][removeLine] = false;
-                }
-                for (int i = 0; i < BOARD_WIDTH; i++) {
-                    for (int j = removeLine; j < BOARD_HEIGHT - 1; j++) {
-                        filledGrid[i][j] = filledGrid[i][j + 1];
-                    }
-                }
-                removeLine = -1;
-                drawFilledGrid();
-            }
-            System.out.println(Arrays.toString(counter));
-            currentBlock = new Figure();
-            currentX = BOARD_WIDTH / 2 + 1;
-            currentY = BOARD_HEIGHT - 1 + currentBlock.minY();
-        }
-        drawFilledGrid();
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
-            return;
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
-            moveBlock(currentX + 1, currentY + 1);
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
-            moveBlock(currentX - 1, currentY + 1);
-        }
-        drawBlock(currentBlock, currentX, currentY);
-        moveBlock(currentX, currentY - 1);
+        batch.draw(boardTexture, 0, 0);
+//        drawGrid();
+//        if (isDown) {
+//            isDown = false;
+//            int removeLine = -1;
+//            for (int i = 0; i < 4; i++) {
+//                int x = currentX + currentBlock.getX(i);
+//                int y = currentY + currentBlock.getY(i);
+//                filledGrid[x - 1][y - currentBlock.minY()] = true;
+//            }
+//            int counter[] = new int[ARRAY_HEIGHT];
+//            for (int i = 0; i < ARRAY_WIDTH; i++) {
+//                for (int j = 0; j < ARRAY_HEIGHT; j++) {
+//                    if (filledGrid[i][j]) {
+//                        counter[j]++;
+//                    }
+//                    if (counter[j] == 10)
+//                        removeLine = j;
+//                }
+//            }
+//            if (removeLine >= 0) {
+//                for (int i = 0; i < ARRAY_WIDTH; i++) {
+//                    filledGrid[i][removeLine] = false;
+//                }
+//                for (int i = 0; i < ARRAY_WIDTH; i++) {
+//                    for (int j = removeLine; j < ARRAY_HEIGHT - 1; j++) {
+//                        filledGrid[i][j] = filledGrid[i][j + 1];
+//                    }
+//                }
+//                removeLine = -1;
+//                drawFilledGrid();
+//            }
+//            System.out.println(Arrays.toString(counter));
+//            currentBlock = new Figure();
+//            currentX = ARRAY_WIDTH / 2 + 1;
+//            currentY = ARRAY_HEIGHT - 1 + currentBlock.minY();
+//        }
+//        drawFilledGrid();
+//        try {
+//            Thread.sleep(300);
+//        } catch (InterruptedException e) {
+//            return;
+//        }
+//        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
+//            moveBlock(currentX + 1, currentY + 1);
+//        }
+//        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
+//            moveBlock(currentX - 1, currentY + 1);
+//        }
+//        drawBlock(currentBlock, currentX, currentY);
+//        moveBlock(currentX, currentY - 1);
     }
 
     public void drawBlock(Figure sq, float x, float y) {
-        x = ((x - 1) / 10 * WIDTH) + 490;
-        y = (y - currentBlock.minY()) / 22 * HEIGHT;
+        x = ((x - 1) / 10 * PIXEL_WIDTH) + 490;
+        y = (y - currentBlock.minY()) / 22 * PIXEL_HEIGHT;
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(1, 0, 0, 1);
         for (int i = 0; i < 4; i++) {
@@ -109,17 +107,17 @@ public class Board implements IBoard {
     public void drawGrid() {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.setColor(1, 1, 0, 1);
-        for (int i = 1280 / 2 - WIDTH / 2; i < 1280 / 2 + WIDTH / 2; i += step) {
-            for (int j = 0; j < HEIGHT; j += step)
+        for (int i = 1280 / 2 - PIXEL_WIDTH / 2; i < 1280 / 2 + PIXEL_WIDTH / 2; i += step) {
+            for (int j = 0; j < PIXEL_HEIGHT; j += step)
                 shapeRenderer.rect(i, j, step, step);
         }
         shapeRenderer.end();
     }
 
     public void moveBlock(int x, int y) {
-        if (x > 0 && x < BOARD_WIDTH)
+        if (x > 0 && x < ARRAY_WIDTH)
             currentX = x;
-        for (int i = 0; i < BOARD_HEIGHT; i++) {
+        for (int i = 0; i < ARRAY_HEIGHT; i++) {
             if (filledGrid[x - 1][i] && y <= i) {
                 isDown = true;
                 return;
@@ -134,8 +132,8 @@ public class Board implements IBoard {
         boolean removeLine = false;
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(0, 0, 1, 1);
-        for (int i = 1280 / 2 - WIDTH / 2, k = 0; i < 1280 / 2 + WIDTH / 2; i += step, k++) {
-            for (int j = 0, l = 0; j < HEIGHT; j += step, l++) {
+        for (int i = 1280 / 2 - PIXEL_WIDTH / 2, k = 0; i < 1280 / 2 + PIXEL_WIDTH / 2; i += step, k++) {
+            for (int j = 0, l = 0; j < PIXEL_HEIGHT; j += step, l++) {
                 if (filledGrid[k][l]) {
                     shapeRenderer.rect(i, j, step, step);
                 }
@@ -145,9 +143,9 @@ public class Board implements IBoard {
     }
 
     private void createBoardTexture() {
-        Pixmap pixmap = new Pixmap(WIDTH, HEIGHT, Pixmap.Format.RGBA8888);
+        Pixmap pixmap = new Pixmap(PIXEL_WIDTH, PIXEL_HEIGHT, Pixmap.Format.RGBA8888);
         pixmap.setColor(Color.SKY);
-        pixmap.drawRectangle(0, 0, WIDTH, HEIGHT);
+        pixmap.drawRectangle(0, 0, PIXEL_WIDTH, PIXEL_HEIGHT);
         boardTexture = new Texture(pixmap);
     }
 }
