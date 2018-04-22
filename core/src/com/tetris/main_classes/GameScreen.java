1d0bc9a9c8b32862e89ec99bd9cbc85240bc2bb8
Amadeusz <xaaq333@gmail.com>
2018-04-22 02:22:58 +0200

    split render method and implement key pressing in GameScreen

--- core/src/com/tetris/main_classes/GameScreen.java
+++ core/src/com/tetris/main_classes/GameScreen.java
@@ -1,13 +1,17 @@
 package com.tetris.main_classes;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.tetris.game_utils.Board;
 
 import java.util.ArrayList;
 
 public class GameScreen implements Screen {
-    private final MainController controller;
+    private static final float TIME_BETWEEN_FRAMES = 0.5f;
+    private MainController controller;
     private ArrayList<Board> boardArray = new ArrayList<>();
+    private float timeSinceLastFrame = 0;
 
     GameScreen(MainController controller, int boardNumber) {
         this.controller = controller;
@@ -17,15 +21,15 @@ public class GameScreen implements Screen {
     }
 
     @Override
-    public void show() {
+    public void render(float delta) {
+        timeSinceLastFrame += delta;
+        handleKeyboardPress();
+        updateBoardsIfTimePassed();
+        drawBoards();
     }
 
     @Override
-    public void render(float delta) {
-        for (Board board : boardArray) {
-            board.update();
-            board.draw(controller.batch);
-        }
+    public void show() {
     }
 
     @Override
@@ -47,4 +51,32 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
     }
+
+    private void handleKeyboardPress() {
+        int pressedKey = 0;
+        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT))
+            pressedKey = Input.Keys.LEFT;
+        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT))
+            pressedKey = Input.Keys.RIGHT;
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE))
+            pressedKey = Input.Keys.SPACE;
+        if (pressedKey != 0)
+            for (Board board : boardArray)
+                board.handleKeyPress(pressedKey);
+    }
+
+    private void updateBoardsIfTimePassed() {
+        if (timeSinceLastFrame >= TIME_BETWEEN_FRAMES) {
+            timeSinceLastFrame -= TIME_BETWEEN_FRAMES;
+            for (Board board : boardArray)
+                board.update();
+        }
+    }
+
+    private void drawBoards() {
+        for (Board board : boardArray) {
+            board.draw(controller.batch);
+        }
+    }
+
 }
