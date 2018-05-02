ae5ca4fe5f73586deb68616ad821a8e49e85da05
NarQiu <magma1magma@gmail.com>
2018-05-02 18:02:06 +0200

    Added dependency to Junit run tests.

---
+++ core/src/test/java/com/tetris/game_utils/SquareTest.java
@@ -0,0 +1,36 @@
+package com.tetris.game_utils;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.xml.soap.Text;
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class SquareTest {
+
+    private Square testSquare;
+    private SpriteBatch testBatch;
+    @Before
+    public void setUp(){
+        //testBatch = new SpriteBatch();
+        //testSquare = new Square(3,3); //nie rozumiem dlaczego to nie działa...
+    }
+
+    @Test
+    public void textureCanBeDrawn()
+    {
+/*        SpriteBatch spriteBatch = new SpriteBatch();
+        Pixmap pixmap = new Pixmap(10, 10, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.SKY);
+        Texture texture = new Texture(pixmap);
+        spriteBatch.begin();
+        spriteBatch.draw(texture, 0f, 0f);
+        spriteBatch.end();*/
+    }
+}
