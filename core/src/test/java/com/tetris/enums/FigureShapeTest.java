82d502e060d01a3d80618a3bd44ebb8a8bc1b349
NarQiu <magma1magma@gmail.com>
2018-05-03 18:55:41 +0200

    Revert "FigureShape test"
    This reverts commit cf5cb20d854c5c14326dcbf5c402629783443bdd.

--- core/src/test/java/com/tetris/enums/FigureShapeTest.java
+++ core/src/test/java/com/tetris/enums/FigureShapeTest.java
@@ -1,21 +1,12 @@
 package com.tetris.enums;
 
 import org.junit.Test;
-import java.awt.*;
 
 import static org.junit.Assert.*;
 
 public class FigureShapeTest {
 
     @Test
-    public void testFigureShapeI() {
-        Point[] p = new Point[4];
-        p[0] = new Point(0,-1);
-        p[1] = new Point(0,0);
-        p[2] = new Point(0,1);
-        p[3] = new Point(0,2);
-        assertArrayEquals(FigureShape.I.getSquareCoordinatesArray(),p);
+    public void name() {
     }
-
-
 }
