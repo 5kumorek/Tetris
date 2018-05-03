41b8ae66be1ef3d3aa9ec0d1130bab6c09c15cdc
NarQiu <magma1magma@gmail.com>
2018-05-03 12:18:31 +0200

    Added dependency to EasyMock.

--- core/src/test/java/com/tetris/game_utils/BoardTest.java
+++ core/src/test/java/com/tetris/game_utils/BoardTest.java
@@ -1,9 +1,15 @@
 package com.tetris.game_utils;
 
+import com.badlogic.gdx.Input;
+import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
+import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
+
 
 public class BoardTest {
 
@@ -28,10 +34,21 @@ public class BoardTest {
 
     }
 
-    //(expected = IllegalArgumentException.class)
     @Test
     public void sayThat1BoardsIsGood(){
-          //new Board(1);
+        ArrayList<Board> boardArray = new ArrayList<>();
+        boardArray.add(createNiceMock(Board.class));
+        boardArray.get(0).update();
+        //assertTrue(true);
     }
+
+    @Test
+    public void sayThat2BoardsIsGood(){
+        Board b = EasyMock.createMockBuilder(Board.class).withConstructor(1).createMock();
+        //java.lang.UnsatisfiedLinkError
+
+        assertTrue(true);
+    }
+
 
 }
