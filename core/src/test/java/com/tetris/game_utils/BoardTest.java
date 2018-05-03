d51687033b7aa59cf7797094f180d9973f9db4e1
NarQiu <magma1magma@gmail.com>
2018-05-03 19:24:46 +0200

    restoring the previous test state

---
+++ core/src/test/java/com/tetris/game_utils/BoardTest.java
@@ -0,0 +1,39 @@
+package com.tetris.game_utils;
+
+import com.badlogic.gdx.Input;
+import org.easymock.EasyMock;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.*;
+
+
+/*
+public class BoardTest {
+
+    @Test
+    public void sayThat0BoardsIsBad(){
+
+    }
+
+    @Test
+    public void sayThat1BoardsIsGood(){
+        ArrayList<Board> boardArray = new ArrayList<>();
+        boardArray.add(createNiceMock(Board.class));
+        boardArray.get(0).update();
+        //assertTrue(true);
+    }
+
+    @Test
+    public void sayThat2BoardsIsGood(){
+        //Board b = EasyMock.createMockBuilder(Board.class).withConstructor(1).createMock();
+        //java.lang.UnsatisfiedLinkError
+
+        assertTrue(true);
+    }
+
+
+}*/
---
+++ core/src/test/java/com/tetris/game_utils/FigureFactoryTest.java
@@ -0,0 +1,24 @@
+package com.tetris.game_utils;
+
+import com.tetris.enums.FigureShape;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static com.tetris.game_utils.Board.ARRAY_HEIGHT;
+import static com.tetris.game_utils.Board.ARRAY_WIDTH;
+import static org.easymock.EasyMock.createNiceMock;
+import static org.junit.Assert.*;
+
+/*
+public class FigureFactoryTest {
+
+    @Test
+    public void testGetFigure() {
+        FigureShape[] figureShapeValues = FigureShape.values();
+        int randomNumber = new Random().nextInt(figureShapeValues.length);
+        FigureShape randomFigureShape = figureShapeValues[randomNumber];
+        FigureFactory figureFactory = createNiceMock(FigureFactory.class);
+        Figure currentFigure = figureFactory.getFigure(ARRAY_WIDTH / 2, ARRAY_HEIGHT - 1, randomFigureShape);
+    }
+}*/
