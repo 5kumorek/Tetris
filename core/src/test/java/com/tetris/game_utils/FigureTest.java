64c24638f8fe2b02e13bb1f95f5d0a27670f73ab
NarQiu <magma1magma@gmail.com>
2018-05-04 10:24:23 +0200

    added PowerMock (Java mock framework)

--- core/src/test/java/com/tetris/game_utils/FigureTest.java
+++ core/src/test/java/com/tetris/game_utils/FigureTest.java
@@ -2,16 +2,20 @@ package com.tetris.game_utils;
 
 import com.tetris.enums.FigureShape;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.*;
 import java.awt.*;
+import org.powermock.modules.junit4.*;
 
 import static org.junit.Assert.*;
 
+
+@RunWith(PowerMockRunner.class)
 public class FigureTest {
 
     @Test
     public void test() {
-/*        Point p = new Point(1,1);
+/*       Point p = new Point(1,1);
         FigureShape[] figureShapeValues = FigureShape.values();
         FigureShape figureShape = figureShapeValues[1];
         Point[] pointArray = figureShape.getSquareCoordinatesArray();
