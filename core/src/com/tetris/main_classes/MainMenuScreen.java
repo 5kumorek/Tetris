585516c2644818b7de6e06dba1e45b3052093c91
Shpiler7 <pcts79@gmail.com>
2018-05-10 13:21:33 +0200

    added additional simple buttons

--- core/src/com/tetris/main_classes/MainMenuScreen.java
+++ core/src/com/tetris/main_classes/MainMenuScreen.java
@@ -8,16 +8,22 @@ public class MainMenuScreen implements Screen {
     private final MainController controller;
     private SpriteBatch batch = new SpriteBatch();
     private Button startButton;
+    private Button optionsButton;
+    private Button exitButton;
 
     MainMenuScreen(MainController controller) {
         this.controller = controller;
         startButton = new Button("start_button.png");
+        optionsButton = new Button("options_button.png");
+        exitButton = new Button("exit_button.png");
     }
 
     @Override
     public void render(float delta) {
         batch.begin();
         batch.draw(startButton.getButtonTexture(), 680 ,440, Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT);
+        batch.draw(optionsButton.getButtonTexture(), 680 ,340, Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT);
+        batch.draw(exitButton.getButtonTexture(), 680 ,240, Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT);
         controller.font.draw(batch, "Tetris", 680, 700);
         batch.end();
     }
