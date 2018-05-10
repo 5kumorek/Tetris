ab7ea102ec6f8924153c8908fbb15195b54fa850
Shpiler7 <pcts79@gmail.com>
2018-05-10 17:25:30 +0200

    added active buttons and functionality of start button and exit button

--- core/src/com/tetris/gui/Button.java
+++ core/src/com/tetris/gui/Button.java
@@ -6,15 +6,19 @@ public class Button {
     public static final int BUTTON_WIDTH = 300;
     public static final int BUTTON_HEIGHT = 100;
     private Texture buttonTexture;
+    private Texture buttonActiveTexture;
+    private static final String EXTENSION = ".png";
 
     public Button(String path)
     {
-        buttonTexture = new Texture(path);
+        buttonTexture = new Texture(path + EXTENSION);
+        buttonActiveTexture = new Texture(path + "_active" + EXTENSION);
     }
 
     public Texture getButtonTexture()
     {
         return buttonTexture;
     }
+    public Texture getButtonActiveTexture() { return buttonActiveTexture; }
 }
 //TODO: zrobic ta klase buttona (to ma byc button ktory bedzie znajdowal w menu glownym)
--- core/src/com/tetris/main_classes/MainMenuScreen.java
+++ core/src/com/tetris/main_classes/MainMenuScreen.java
@@ -1,5 +1,6 @@
 package com.tetris.main_classes;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.tetris.gui.Button;
@@ -13,18 +14,43 @@ public class MainMenuScreen implements Screen {
 
     MainMenuScreen(MainController controller) {
         this.controller = controller;
-        startButton = new Button("start_button.png");
-        optionsButton = new Button("options_button.png");
-        exitButton = new Button("exit_button.png");
+        startButton = new Button("start_button");
+        optionsButton = new Button("options_button");
+        exitButton = new Button("exit_button");
     }
 
     @Override
     public void render(float delta) {
+        int xButtonStart = 680;
+        int yButtonStart = 240;
         batch.begin();
-        batch.draw(startButton.getButtonTexture(), 680 ,440, Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT);
-        batch.draw(optionsButton.getButtonTexture(), 680 ,340, Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT);
-        batch.draw(exitButton.getButtonTexture(), 680 ,240, Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT);
-        controller.font.draw(batch, "Tetris", 680, 700);
+        if(Gdx.input.getX() > xButtonStart && Gdx.input.getX() < xButtonStart + Button.BUTTON_WIDTH && 720 - Gdx.input.getY() > yButtonStart+200 && 720 - Gdx.input.getY() < yButtonStart+200+Button.BUTTON_HEIGHT) {
+            batch.draw(startButton.getButtonActiveTexture(), xButtonStart, yButtonStart+200, Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT);
+            if(Gdx.input.isTouched()){
+                controller.setScreen(new GameScreen(controller, 6));
+            }
+        }
+        else {
+            batch.draw(startButton.getButtonTexture(), xButtonStart, yButtonStart+200, Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT);
+        }
+        if(Gdx.input.getX() > xButtonStart && Gdx.input.getX() < xButtonStart + Button.BUTTON_WIDTH && 720 - Gdx.input.getY() > yButtonStart+100 && 720 - Gdx.input.getY() < yButtonStart+100+Button.BUTTON_HEIGHT) {
+            batch.draw(optionsButton.getButtonActiveTexture(), xButtonStart, yButtonStart+100, Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT);
+            if(Gdx.input.isTouched()){
+            }
+        }
+        else {
+            batch.draw(optionsButton.getButtonTexture(), xButtonStart, yButtonStart+100, Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT);
+        }
+        if(Gdx.input.getX() > xButtonStart && Gdx.input.getX() < xButtonStart + Button.BUTTON_WIDTH && 720 - Gdx.input.getY() > yButtonStart && 720 - Gdx.input.getY() < yButtonStart+Button.BUTTON_HEIGHT) {
+            batch.draw(exitButton.getButtonActiveTexture(), xButtonStart, yButtonStart, Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT);
+            if(Gdx.input.isTouched()){
+                Gdx.app.exit();
+            }
+        }
+        else {
+            batch.draw(exitButton.getButtonTexture(), xButtonStart, yButtonStart, Button.BUTTON_WIDTH, Button.BUTTON_HEIGHT);
+        }
+        controller.font.draw(batch, "Tetris", xButtonStart, 700);
         batch.end();
     }
 
