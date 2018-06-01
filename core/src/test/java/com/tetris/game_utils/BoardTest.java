3e4b2f4c7d18276fc45637b2a0312ef6a23029ab
NarQiu <magma1magma@gmail.com>
2018-06-01 23:11:25 +0200

    added tests of filled rows removal

--- core/src/test/java/com/tetris/game_utils/BoardTest.java
+++ core/src/test/java/com/tetris/game_utils/BoardTest.java
@@ -3,24 +3,25 @@ package com.tetris.game_utils;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.tetris.enums.FigureShape;
-import org.easymock.EasyMock;
-import org.easymock.Mock;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mockito;
-import org.powermock.api.easymock.PowerMock;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.junit.internal.runners.statements.ExpectException;
+import org.junit.rules.ExpectedException;
 
-import javax.xml.stream.FactoryConfigurationError;
+import java.io.EOFException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 
+import static com.tetris.game_utils.Board.ARRAY_HEIGHT;
 import static com.tetris.game_utils.Board.ARRAY_WIDTH;
+import static java.lang.System.out;
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.willAnswer;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 
 public class BoardTest extends GameTestBase{
@@ -28,7 +29,6 @@ public class BoardTest extends GameTestBase{
     @Test
     public void createSimpleBoard(){
         new Board(1, createNiceMock(SpriteBatch.class));
-        //board.handleKeyPress(Input.Keys.LEFT);
     }
 
     @Test
@@ -42,7 +42,7 @@ public class BoardTest extends GameTestBase{
             field.setAccessible(true);
             field.set(b,f);
         }catch (NoSuchFieldException | IllegalAccessException e){
-            System.out.println(e.toString());
+            out.println(e.toString());
         }
         b.handleKeyPress(Input.Keys.LEFT);
         Square[] square = f.getSquareArray();
@@ -60,7 +60,7 @@ public class BoardTest extends GameTestBase{
             field.setAccessible(true);
             field.set(b,f);
         }catch (NoSuchFieldException | IllegalAccessException e){
-            System.out.println(e.toString());
+            out.println(e.toString());
         }
         b.handleKeyPress(Input.Keys.RIGHT);
         Square[] square = f.getSquareArray();
@@ -77,7 +77,7 @@ public class BoardTest extends GameTestBase{
             field.setAccessible(true);
             field.set(b,f);
         }catch (NoSuchFieldException | IllegalAccessException e){
-            System.out.println(e.toString());
+            out.println(e.toString());
         }
         b.handleKeyPress(Input.Keys.SPACE);
         Square[] square = f.getSquareArray();
@@ -95,7 +95,7 @@ public class BoardTest extends GameTestBase{
             assertNotNull(field.get(b));
         }catch (NoSuchFieldException | IllegalAccessException e){
             fail("Exception was thrown");
-            System.out.println(e.toString());
+            out.println(e.toString());
         }
     }
 
@@ -109,7 +109,7 @@ public class BoardTest extends GameTestBase{
             assertNotNull(field.get(b));
         }catch (NoSuchFieldException | IllegalAccessException e){
             fail("Exception was thrown");
-            System.out.println(e.toString());
+            out.println(e.toString());
         }
     }
     /*
@@ -140,7 +140,7 @@ public class BoardTest extends GameTestBase{
             field.setAccessible(true);
             field.set(b,f);
         }catch (NoSuchFieldException | IllegalAccessException e){
-            System.out.println(e.toString());
+            out.println(e.toString());
         }
         b.update();
         try {
@@ -149,7 +149,7 @@ public class BoardTest extends GameTestBase{
             assertNull(field.get(b));
         }catch (NoSuchFieldException | IllegalAccessException e){
             fail("Exception was thrown");
-            System.out.println(e.toString());
+            out.println(e.toString());
         }
     }
 
@@ -163,7 +163,7 @@ public class BoardTest extends GameTestBase{
             field.setAccessible(true);
             field.set(b,f);
         }catch (NoSuchFieldException | IllegalAccessException e){
-            System.out.println(e.toString());
+            out.println(e.toString());
         }
         b.update();
         try {
@@ -172,9 +172,79 @@ public class BoardTest extends GameTestBase{
             assertNotNull(field.get(b));
         }catch (NoSuchFieldException | IllegalAccessException e){
             fail("Exception was thrown");
-            System.out.println(e.toString());
+            out.println(e.toString());
         }
     }
 
-    
+    /*
+    @Test // Niestety System.exit(0) przerywa działanie testu
+    public void loseGameShouldBeCalled(){
+        Board d = new Board(1, createNiceMock(SpriteBatch.class));
+        Board b = spy(d);
+        FigureFactory figureFactory = new FigureFactory();
+        Figure f = figureFactory.getFigure(ARRAY_WIDTH / 2, ARRAY_HEIGHT - 1, FigureShape.I);
+        try {
+            Square[] figureSquareArray = f.getSquareArray();
+            ArrayList<Square> squareArray = new ArrayList<>();
+            squareArray.addAll(Arrays.asList(figureSquareArray));
+            Field field = Board.class.getDeclaredField("squareArray");
+            field.setAccessible(true);
+            field.set(b,squareArray);
+            Method method = Board.class.getDeclaredMethod("loseGame");
+            method.setAccessible(true);
+            //when(Board.class.getDeclaredMethod("createSquareTextre")).thenThrow(new RuntimeException());
+            when(Board.class.getDeclaredMethod("loseGame")).thenThrow(new RuntimeException());
+        }catch (NoSuchFieldException | IllegalAccessException | NoSuchMethodException e){
+            out.println(e.toString());
+        }
+            b.update();
+    }
+    */
+
+    @Test//Unchecked Cast can be avoided because of type erasure
+    public @SuppressWarnings("unchecked") void shouldDeleteFilledRows(){
+        Board b = new Board(1, createNiceMock(SpriteBatch.class));
+        try {
+            ArrayList<Square> squareArray = new ArrayList<>();
+            for(int i = 0; i < ARRAY_WIDTH; i++){
+                squareArray.add(new Square(i,0));
+            }
+            Field field = Board.class.getDeclaredField("squareArray");
+            field.setAccessible(true);
+            field.set(b,squareArray);
+            Method method = Board.class.getDeclaredMethod("deleteFilledRows");
+            method.setAccessible(true);
+            method.invoke(b);
+            ArrayList<Square> s = (ArrayList<Square>) field.get(b);
+            assertTrue(s.isEmpty());
+        }catch (NoSuchFieldException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e){
+            out.println(e.toString());
+        }
+        b.update();
+    }
+
+    @Test //Unchecked Cast can be avoided because of type erasure
+    public @SuppressWarnings("unchecked") void shouldDeleteFilledRowAndMoveDown(){
+        Board b = new Board(1, createNiceMock(SpriteBatch.class));
+        try {
+            ArrayList<Square> squareArray = new ArrayList<>();
+            for(int i = 0; i < ARRAY_WIDTH; i++){
+                squareArray.add(new Square(i,0));
+            }
+            squareArray.add(new Square(ARRAY_WIDTH/2,ARRAY_HEIGHT/2));
+            Field field = Board.class.getDeclaredField("squareArray");
+            field.setAccessible(true);
+            field.set(b,squareArray);
+            Method method = Board.class.getDeclaredMethod("deleteFilledRows");
+            method.setAccessible(true);
+            method.invoke(b);
+            ArrayList<Square> s = (ArrayList<Square>) field.get(b);
+            assertEquals(s.get(0).getY(),ARRAY_HEIGHT / 2 - 1);
+            assertEquals(s.get(0).getX(), ARRAY_WIDTH / 2);
+        }catch (NoSuchFieldException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e){
+            out.println(e.toString());
+        }
+        b.update();
+    }
+
 }
