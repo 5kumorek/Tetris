3cce8dcb2adcd5293e7f2d68a098d3c3028854a9
Shpiler7 <pcts79@gmail.com>
2018-06-07 15:02:26 +0200

    fixed TopScoresScreen

--- core/src/com/tetris/main_classes/TopScoresScreen.java
+++ core/src/com/tetris/main_classes/TopScoresScreen.java
@@ -1,5 +1,6 @@
 package com.tetris.main_classes;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -12,10 +13,14 @@ public class TopScoresScreen implements Screen
     private final MainController controller;
     private SpriteBatch batch = new SpriteBatch();
     private int topScores [][];
+    private int height;
+    private int width;
 
-    public TopScoresScreen(MainController controller) {
+    TopScoresScreen(MainController controller) {
         this.controller = controller;
         topScores = new int[6][10];
+        height = Gdx.graphics.getHeight();
+        width = Gdx.graphics.getWidth();
     }
 
     @Override
@@ -73,12 +78,12 @@ public class TopScoresScreen implements Screen
 
     }
 
-    public void drawScore(int boardNumber)  {
+    private void drawScore(int boardNumber)  {
         batch.begin();
-        controller.font.draw(batch, "Number of boards: " + (boardNumber+1), 300+boardNumber*200, 600);
+        controller.font.draw(batch, "Number of boards: " + (boardNumber+1), width/10+boardNumber*(width/7), height-height/11);
         for(int i = 0; i < 10; i++)
         {
-            controller.font.draw(batch, (i+1) + ".  " + topScores[boardNumber][i], 350+boardNumber*200, 550-i*50);
+            controller.font.draw(batch, (i+1) + ".  " + topScores[boardNumber][i], width/10+width/30+boardNumber*(width/7), height-height/8-i*height/12);
         }
         batch.end();
     }
