943de79e7367c9f7f4072b7017a77e49749e58a0
Amadeusz <xaaq333@gmail.com>
2018-06-08 00:06:27 +0200

    create docstrings in FigureFactory

--- core/src/com/tetris/game_utils/FigureFactory.java
+++ core/src/com/tetris/game_utils/FigureFactory.java
@@ -4,13 +4,27 @@ import com.tetris.enums.FigureShape;
 
 import java.awt.*;
 
+/**
+ * Factory of figures.
+ */
 class FigureFactory {
     private int squareColor;
 
+    /**
+     * Initializes color of created figures.
+     * @param squareColor color of figures to create
+     */
     FigureFactory(int squareColor){
         this.squareColor = squareColor;
     }
 
+    /**
+     * Creates new figure.
+     * @param centerX center x coordinate of new figure
+     * @param centerY center y coordinate of new figure
+     * @param figureShape shape of new figure
+     * @return new figure
+     */
     Figure getFigure(int centerX, int centerY, FigureShape figureShape) {
         Point[] pointArray = figureShape.getSquareCoordinatesArray();
         pointArray = translatePoints(centerX, centerY, pointArray);
@@ -18,12 +32,24 @@ class FigureFactory {
         return new Figure(centerX, centerY, squareArray);
     }
 
+    /**
+     * Translates points of given array by given translation.
+     * @param xTranslation x translation
+     * @param yTranslation y translation
+     * @param pointArray array from which to translate points
+     * @return array of translated points
+     */
     private Point[] translatePoints(int xTranslation, int yTranslation, final Point[] pointArray) {
         for (Point point : pointArray)
             point.translate(xTranslation, yTranslation);
         return pointArray;
     }
 
+    /**
+     * Converts array of points to array of squares.
+     * @param pointArray array of points to convert
+     * @return array of squares converted from points
+     */
     private Square[] convertPointsToSquares(Point[] pointArray) {
         Square[] squareArray = new Square[pointArray.length];
 
