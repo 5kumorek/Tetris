06e0a395284e20d5addcf304d35039f0ff9d81a5
Amadeusz <xaaq333@gmail.com>
2018-06-07 23:40:26 +0200

    add docstrings in Direction

--- core/src/com/tetris/enums/Direction.java
+++ core/src/com/tetris/enums/Direction.java
@@ -1,5 +1,8 @@
 package com.tetris.enums;
 
+/**
+ * Class used to encapsulate directions in which figures can move.
+ */
 public enum Direction {
     UP(0, 1),
     DOWN(0, -1),
@@ -8,15 +11,31 @@ public enum Direction {
 
     private final int x, y;
 
+    /**
+     * Constructor for given enum example.
+     *
+     * @param x x coordinate
+     * @param y y coordinate
+     */
     Direction(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Getter for x coordinate.
+     *
+     * @return x coordinate
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Getter for y coordinate.
+     *
+     * @return y coordinate
+     */
     public int getY() {
         return y;
     }
