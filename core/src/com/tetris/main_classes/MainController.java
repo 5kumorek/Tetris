d6d9f041849b0117ecd1c993ba237f377a5b8fb9
Shpiler7 <pcts79@gmail.com>
2018-06-10 14:47:28 +0200

    Added docstrings in MainController

--- core/src/com/tetris/main_classes/MainController.java
+++ core/src/com/tetris/main_classes/MainController.java
@@ -5,9 +5,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
+/**
+ * Class that allows an application to easily have multiple screens
+ */
 public class MainController extends Game {
     BitmapFont font;
 
+    /**
+     * Starts first screen
+     */
     public void create() {
         font = new BitmapFont();
         Gdx.graphics.setTitle("Tetris");
@@ -25,6 +31,9 @@ public class MainController extends Game {
         font.dispose();
     }
 
+    /**
+     * Clears screen
+     */
     private void clearScreen() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
